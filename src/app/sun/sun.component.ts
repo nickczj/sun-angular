e951bc94509604563b1a9383f1088af3357bddfb
nczj <Nicholas.Chow@sg.bosch.com>
2019-01-16 17:51:41 +0800

    add interface for sun position

--- src/app/sun/sun.component.ts
+++ src/app/sun/sun.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as SunCalc from 'suncalc';
+import { Position } from './position.interface'
 
 @Component({
   selector: 'app-sun',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SunComponent implements OnInit {
 
+  currentSunPosition: Position;
+
   constructor() { }
 
   ngOnInit() {
+    this.currentSunPosition = SunCalc.getPosition(new Date(), 1.35254, 103.9588412);
+    console.log(this.currentSunPosition);
   }
 
 }

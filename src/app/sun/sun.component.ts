1b7110f6832a246e5bb08f898f51f6223d390999
Nicholas Chow <nick.chow.zj@gmail.com>
2019-01-19 17:05:12 +0800

    add three.js

--- src/app/sun/sun.component.ts
+++ src/app/sun/sun.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as SunCalc from 'suncalc';
 import { Position } from './position.interface';
+import * as THREE from 'three-full';
 
 @Component({
   selector: 'app-sun',
@@ -78,5 +79,37 @@ export class SunComponent implements OnInit {
       };
     }
   }
+
+  // createSphere() {
+  //   console.log("1");
+  //   var scene = new THREE.Scene;
+  //   var camera = new THREE.PerspectiveCamera(50, 500 / 400, 0.1, 1000);
+    
+  //   console.log("2");
+
+  //   var renderer = new THREE.WebGLRenderer();
+  //   renderer.setSize(500, 400);
+  //   document.body.appendChild(renderer.domElement);
+    
+  //   console.log("3");
+
+  //   var geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+  //   var material = new THREE.MeshNormalMaterial();
+  //   var cube = new THREE.Mesh(geometry, material);
+  //   scene.add(cube);
+    
+    
+  //   camera.position.z = 10;
+  //   var render = function () {
+  //       requestAnimationFrame(render);
+    
+  //       // cube.rotateX();
+  //       cube.rotation.y += 0.01;
+    
+  //       renderer.render(scene, camera);
+  //   };
+    
+  //   render();
+  // }
 
 }
